669644f907ed46af088ad351e61baea333ee344e
ciosyam <e1121211047@student.untan.ac.id>
2024-03-22 02:42:46 +0800

    22maret

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
 #define OBSIDIAN "obsidian"
 
 /* appearance */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
+static unsigned int borderpx  = 0;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 10;       /* vert inner gap between windows */
