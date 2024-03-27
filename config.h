f88dd02f2dbf8e8ff737f1637e492edb59fe50e3
ciosyam <e1121211047@student.untan.ac.id>
2024-03-28 04:08:40 +0800

    28maret

--- config.h
+++ config.h
@@ -13,9 +13,9 @@ static unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
-static int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
+static int showbar            = 0;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "TerminessNerdFontMono:size=10:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#000000";
